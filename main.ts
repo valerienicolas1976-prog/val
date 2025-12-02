1d34bade224f134030c60b64df5ee196c10ba693
valerienicolas1976-prog <valerienicolas1976@gmail.com>
2025-12-02 09:54:14 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(500)
+    basic.showIcon(IconNames.Heart)
+})
